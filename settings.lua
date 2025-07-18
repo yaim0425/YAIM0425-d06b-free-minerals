a0bd02cc514326e130ec0a9743f66baf9e107190
yaim0425 <yaim0425@gmail.com>
2025-07-18 05:46:02 -0500

    Primer commit

---
+++ settings.lua
@@ -0,0 +1,11 @@
+data:extend({
+	{
+		type = "int-setting",
+		name = "zzzYAIM0425-0600-quantity",
+		order = "1",
+		setting_type = "startup",
+		minimum_value = 100,
+		maximum_value = 65000,
+		default_value = 500
+	}
+})
