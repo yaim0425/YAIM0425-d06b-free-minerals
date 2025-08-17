c14f75f9637d0b8f0617132d604f28b276244a51
yaim0425 <yaim0425@gmail.com>
2025-08-17 04:12:46 -0500

    Cambio en setting.lua

--- settings.lua
+++ settings.lua
@@ -1,11 +1,23 @@
 data:extend({
 	{
+		type = "bool-setting",
+		name = "zzzYAIM0425-0600-stack-size",
+		localised_name = { "gui-selector.stack-size" },
+		localised_description = { "",
+			{ "gui.off" }, " -> ", { "gui-blueprint-parametrisation.value" }, " = ", { "description.amount" }, "\n",
+			{ "gui.on" }, " -> ", { "gui-blueprint-parametrisation.value" }, " = ", { "description.amount" }, " x ", { "gui-selector.stack-size" }, "\n",
+			{ "gui-upgrade.module-limit" }, " ", "" .. 65000
+		},
+		order = "1",
+		setting_type = "startup",
+		default_value = false
+	}, {
 		type = "int-setting",
 		name = "zzzYAIM0425-0600-amount",
 		localised_name = { "description.amount" },
-		order = "1",
+		order = "2",
 		setting_type = "startup",
-		minimum_value = 100,
+		minimum_value = 2,
 		maximum_value = 65000,
 		default_value = 500
 	}
