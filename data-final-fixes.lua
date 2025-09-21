47948bfaaa34d0303cb3ba522a3a2c480fb3104b
yaim0425 <yaim0425@gmail.com>
2025-09-21 11:12:33 -0500

    Creación de data-final-fixes.lua

---
+++ data-final-fixes.lua
@@ -0,0 +1,182 @@
+---------------------------------------------------------------------------
+---[ data-final-fixes.lua ]---
+---------------------------------------------------------------------------
+
+
+
+
+
+---------------------------------------------------------------------------
+---[ Contenedor de este archivo ]---
+---------------------------------------------------------------------------
+
+local This_MOD = GMOD.get_id_and_name()
+if not This_MOD then return end
+GMOD[This_MOD.id] = This_MOD
+
+---------------------------------------------------------------------------
+
+
+
+
+
+---------------------------------------------------------------------------
+---[ Inicio del MOD ]---
+---------------------------------------------------------------------------
+
+function This_MOD.start()
+    --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+
+    --- Valores de la referencia
+    This_MOD.setting_mod()
+
+    -- --- Obtener los elementos
+    -- This_MOD.get_elements()
+
+    -- --- Modificar los elementos
+    -- for iKey, spaces in pairs(This_MOD.to_be_processed) do
+    --     for jKey, space in pairs(spaces) do
+    --         --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+
+    --         --- Marcar como procesado
+    --         This_MOD.processed[iKey] = This_MOD.processed[iKey] or {}
+    --         This_MOD.processed[iKey][jKey] = true
+
+    --         --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+
+    --         -- --- Crear los elementos
+    --         -- This_MOD.create_item(space)
+    --         -- This_MOD.create_entity(space)
+    --         -- This_MOD.create_recipe(space)
+    --         -- This_MOD.create_tech(space)
+
+    --         --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+    --     end
+    -- end
+
+    --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+end
+
+---------------------------------------------------------------------------
+
+
+
+
+
+---------------------------------------------------------------------------
+---[ Valores de la referencia ]---
+---------------------------------------------------------------------------
+
+function This_MOD.setting_mod()
+    --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+    --- Validar si se cargó antes
+    --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+
+    This_MOD.to_be_processed = {}
+    if This_MOD.processed then return end
+
+    --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+
+
+
+
+
+    --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+    --- Valores de la referencia en todos los MODs
+    --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+
+    --- Contenedor de los elementos que el MOD modoficó o modificará
+    This_MOD.processed = {}
+
+    --- Cargar la configuración
+    This_MOD.setting = GMOD.setting[This_MOD.id]
+
+    --- Indicador del mod
+    This_MOD.delete = {
+        icon = data.raw["virtual-signal"]["signal-deny"].icons[1].icon,
+        scale = 0.5
+    }
+
+    This_MOD.create = {
+        icon = data.raw["virtual-signal"]["signal-check"].icons[1].icon,
+        scale = 0.5
+    }
+
+    This_MOD.indicator = {
+        icon = data.raw["virtual-signal"]["signal-star"].icons[1].icon,
+        scale = 0.25,
+        shift = { 0, -5 }
+    }
+
+    This_MOD.indicator_bg = {
+        icon = data.raw["virtual-signal"]["signal-black"].icons[1].icon,
+        scale = 0.25,
+        shift = { 0, -5 }
+    }
+
+    --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+
+
+
+
+
+    --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+    --- Valores de la referencia en este MOD
+    --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+
+    --- Contenedor de los recursos a afectar
+    This_MOD.resource = {}
+
+    --- Valores de referencia
+    This_MOD.entity_name = "assembling-machine-2"
+    This_MOD.new_entity_name = GMOD.name .. "-free-" .. This_MOD.entity_name
+
+    --- Acciones
+    This_MOD.actions = {
+        delete = "ingredients",
+        create = "results"
+    }
+
+    --- Receta base
+    This_MOD.recipe_base = {
+        type = "recipe",
+        name = "",
+        localised_name = {},
+        localised_description = {},
+        energy_required = 1,
+
+        hide_from_player_crafting = true,
+        category = "crafting-with-fluid",
+        subgroup = "",
+        order = "",
+
+        ingredients = {},
+        results = {}
+    }
+
+    --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+end
+
+---------------------------------------------------------------------------
+
+
+
+
+
+---------------------------------------------------------------------------
+---[ Funciones locales ]---
+---------------------------------------------------------------------------
+
+---------------------------------------------------------------------------
+
+
+
+
+
+---------------------------------------------------------------------------
+---[ Iniciar el MOD ]---
+---------------------------------------------------------------------------
+
+This_MOD.start()
+
+---------------------------------------------------------------------------
